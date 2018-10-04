fbabd051cdbd5d39b347fec3d059ba886105d9bd
sjcl <jrhigasinihonn.0601@gmail.com>
2018-10-04 16:19:29 +0900

    Fix syntax error

--- db/initialize.sql
+++ db/initialize.sql
@@ -3,7 +3,7 @@ GRANT INSERT,SELECT,UPDATE,DELETE ON `signpic_db`.* TO `signpic`@`%`;
 
 CREATE DATABASE IF NOT EXISTS `signpic_db`;
 
-CREATE TABLE IF NOT NOT EXISTS `signpic_db`.`user` (
+CREATE TABLE IF NOT EXISTS `signpic_db`.`user` (
     `uuid`              CHAR(32)    NOT NULL,
     `username`          TINYTEXT    NOT NULL,
     `ip`                TINYTEXT    NOT NULL,
