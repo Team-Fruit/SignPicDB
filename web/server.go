ed67f7cf6a8b27ab0c168b7b86ee4f6237fb7dd4
sjcl <jrhigasinihonn.0601@gmail.com>
2018-10-05 11:09:38 +0900

    validator

--- web/server.go
+++ web/server.go
@@ -5,16 +5,27 @@ import (
 
     "github.com/labstack/echo"
     "github.com/labstack/echo/middleware"
+    "gopkg.in/go-playground/validator.v9"
 )
 
-type User struct {
-    UUID string `query:"id"`
-    UserName string `query:"name"`
-    VersionMod string `query:"vmod"`
-    VersionModMC string `query:"vmodmc"`
-    VersionModForge string `query:"vmodforge"`
-    VersionMC string `query:"vmc"`
-    VersionForge string `query:"vforge"`
+type (
+    User struct {
+        UUID            string `query:"id" validate:"required,len=32"`
+        UserName        string `query:"name" validate:"required"`
+        VersionMod      string `query:"vmod" validate:"required"`
+        VersionModMC    string `query:"vmodmc" validate:"required"`
+        VersionModForge string `query:"vmodforge" validate:"required"`
+        VersionMC       string `query:"vmc" validate:"required"`
+        VersionForge    string `query:"vforge" validate:"required"`
+    }
+
+    CustomValidator struct {
+        validator *validator.Validate
+    }
+)
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+    return cv.validator.Struct(i)
 }
 
 func main() {
@@ -23,15 +34,20 @@ func main() {
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
     
+    e.Validator = &CustomValidator{validator: validator.New()}
+
     e.GET("/", root)
 
     e.Logger.Fatal(e.Start(":8080"))
 }
 
-func root(c echo.Context) error {
+func root(c echo.Context) (err error) {
     u := new(User)
-    if err := c.Bind(u); err != nil {
-        return err
+    if err = c.Bind(u); err != nil {
+        return
+    }
+    if err = c.Validate(u); err != nil {
+        return
     }
     return c.JSON(http.StatusOK, u)
 }
