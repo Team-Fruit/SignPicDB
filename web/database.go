fc818d97b49075071e0e6377b4e7c8de17cab43c
sjcl <jrhigasinihonn.0601@gmail.com>
2018-10-15 00:41:27 +0900

    count

--- web/database.go
+++ web/database.go
@@ -1,48 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "reflect"
+	"fmt"
+	"reflect"
+	"strings"
 )
 
 const user = `REPLACE INTO user (uuid, username, ip, version_mod, version_mod_mc, version_mod_forge, version_mc, version_forge, message) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
 func (u *User) Push() {
-    db.MustExec(user, u.UUID,
-                      u.UserName,
-                      u.IP, u.VersionMod,
-                      u.VersionModMC,
-                      u.VersionModForge,
-                      u.VersionMC,
-                      u.VersionForge,
-                      u.Message)
+	db.MustExec(user, u.UUID,
+		u.UserName,
+		u.IP, u.VersionMod,
+		u.VersionModMC,
+		u.VersionModForge,
+		u.VersionMC,
+		u.VersionForge,
+		u.Message)
 }
 
 func (w *Where) Pull(offset uint64, limit uint64) (u []User, err error) {
-    ws := w.toSql()
-    if ws != "" {
-        err = db.Select(&u, fmt.Sprintf("SELECT * FROM user WHERE %s LIMIT %d,%d", ws, offset, limit))
-    } else {
-        err = db.Select(&u, fmt.Sprintf("SELECT * FROM user LIMIT %d,%d", offset, limit))
-    }
-    return
+	ws := w.toSql()
+	if ws != "" {
+		err = db.Select(&u, fmt.Sprintf("SELECT * FROM user WHERE %s LIMIT %d,%d", ws, offset, limit))
+	} else {
+		err = db.Select(&u, fmt.Sprintf("SELECT * FROM user LIMIT %d,%d", offset, limit))
+	}
+	return
 }
 
 func (w *Where) toSql() string {
-    s := []string{}
+	s := []string{}
 
-    val := reflect.ValueOf(w).Elem()
-    for i := 0; i < val.NumField(); i++ {
-        vf := val.Field(i)
-        tf := val.Type().Field(i)
-        tag := tf.Tag
-        fv := vf.Interface().(string)
-        if fv != "" {
-            s = append(s, fmt.Sprintf("%s%s'%s'", tag.Get("db"), tag.Get("operator"), fv))
-        }
-    }
+	val := reflect.ValueOf(w).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		vf := val.Field(i)
+		tf := val.Type().Field(i)
+		tag := tf.Tag
+		fv := vf.Interface().(string)
+		if fv != "" {
+			s = append(s, fmt.Sprintf("%s%s'%s'", tag.Get("db"), tag.Get("operator"), fv))
+		}
+	}
 
-    return strings.Join(s, " AND ")
+	return strings.Join(s, " AND ")
 }
 
+func UserCount() (c uint64, err error) {
+    err = db.Get(&c, "SELECT count(uuid) FROM user")
+    return
+}
--- web/server.go
+++ web/server.go
@@ -1,130 +1,139 @@
 package main
 
 import (
-    "net/http"
-    "strconv"
+	"net/http"
+	"strconv"
 
-    "github.com/labstack/echo"
-    "github.com/labstack/echo/middleware"
-    "gopkg.in/go-playground/validator.v9"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"gopkg.in/go-playground/validator.v9"
 
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/jmoiron/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 )
 
 type (
-    User struct {
-        UUID            string `db:"uuid" query:"id" validate:"required,len=32"`
-        UserName        string `db:"username" query:"name" validate:"required"`
-        IP              string `db:"ip"`
-        VersionMod      string `db:"version_mod" query:"vmod" validate:"required"`
-        VersionModMC    string `db:"version_mod_mc" query:"vmodmc" validate:"required"`
-        VersionModForge string `db:"version_mod_forge" query:"vmodforge" validate:"required"`
-        VersionMC       string `db:"version_mc" query:"vmc" validate:"required"`
-        VersionForge    string `db:"version_forge" query:"vforge" validate:"required"`
-        Message         string `db:"message"`
-        UpdatedAt       string `db:"updated_at"`
-    }
-    Where struct {
-        UUID            string `query:"id" validate:"omitempty,len=32" db:"uuid" operator:"="`
-        UserName        string `query:"name" db:"username" operator:"="`
-        IP              string `query:"ip" validate:"omitempty,ip" db:"ip" operator:"="`
-        VersionMod      string `query:"vmod" db:"version_mod" operator:"="`
-        VersionModMC    string `query:"vmodmc" db:"version_mod_mc" operator:"="`
-        VersionModForge string `query:"vmodforge" db:"version_mod_forge" operator:"="`
-        VersionMC       string `query:"vmc" db:"version_mc" operator:"="`
-        VersionForge    string `query:"vforge" db:"version_forge" operator:"="`
-        Since           string `query:"since" db:"updated_at" operator:">="`
-        Until           string `query:"until" db:"updated_at" operator:"<="`
+	User struct {
+		UUID            string `db:"uuid" query:"id" validate:"required,len=32"`
+		UserName        string `db:"username" query:"name" validate:"required"`
+		IP              string `db:"ip"`
+		VersionMod      string `db:"version_mod" query:"vmod" validate:"required"`
+		VersionModMC    string `db:"version_mod_mc" query:"vmodmc" validate:"required"`
+		VersionModForge string `db:"version_mod_forge" query:"vmodforge" validate:"required"`
+		VersionMC       string `db:"version_mc" query:"vmc" validate:"required"`
+		VersionForge    string `db:"version_forge" query:"vforge" validate:"required"`
+		Message         string `db:"message"`
+		UpdatedAt       string `db:"updated_at"`
+	}
+	Where struct {
+		UUID            string `query:"id" validate:"omitempty,len=32" db:"uuid" operator:"="`
+		UserName        string `query:"name" db:"username" operator:"="`
+		IP              string `query:"ip" validate:"omitempty,ip" db:"ip" operator:"="`
+		VersionMod      string `query:"vmod" db:"version_mod" operator:"="`
+		VersionModMC    string `query:"vmodmc" db:"version_mod_mc" operator:"="`
+		VersionModForge string `query:"vmodforge" db:"version_mod_forge" operator:"="`
+		VersionMC       string `query:"vmc" db:"version_mc" operator:"="`
+		VersionForge    string `query:"vforge" db:"version_forge" operator:"="`
+		Since           string `query:"since" db:"updated_at" operator:">="`
+		Until           string `query:"until" db:"updated_at" operator:"<="`
+	}
+    Count struct {
+        Count uint64
     }
 
-    CustomValidator struct {
-        validator *validator.Validate
-    }
+	CustomValidator struct {
+		validator *validator.Validate
+	}
 )
 
 var db *sqlx.DB
 
 func (cv *CustomValidator) Validate(i interface{}) error {
-    return cv.validator.Struct(i)
+	return cv.validator.Struct(i)
 }
 
 func main() {
-    db = sqlx.MustConnect("mysql", "signpic:@tcp(db:3306)/signpic_db")
-    defer db.Close()
+	db = sqlx.MustConnect("mysql", "signpic:@tcp(db:3306)/signpic_db")
+	defer db.Close()
 
-    e := echo.New()
+	e := echo.New()
 
-    e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
-    
-    e.Validator = &CustomValidator{validator: validator.New()}
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-    e.GET("/", root)
-    e.GET("/list", list)
+	e.Validator = &CustomValidator{validator: validator.New()}
 
-    e.Logger.Fatal(e.Start(":8080"))
+	e.GET("/", root)
+	e.GET("/list", list)
+    e.GET("/list/count", count)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func root(c echo.Context) (err error) {
-    u := new(User)
-    if err = c.Bind(u); err != nil {
-        return
-    }
-    if err = c.Validate(u); err != nil {
-        return
-    }
+	u := new(User)
+	if err = c.Bind(u); err != nil {
+		return
+	}
+	if err = c.Validate(u); err != nil {
+		return
+	}
 
-    u.IP = c.RealIP()
-    u.Message = ""
+	u.IP = c.RealIP()
+	u.Message = ""
 
-    u.Push()
+	u.Push()
 
-    return c.JSON(http.StatusOK, u)
+	return c.JSON(http.StatusOK, u)
 }
 
 func list(c echo.Context) (err error) {
-    var page, pagesize uint64
-    if pagestr := c.QueryParam("page"); pagestr != "" {
-        if page, err = strconv.ParseUint(pagestr, 10, 32); err != nil {
-            return
-        }
-        if page < 1 {
-            page = 1
-        }
-    } else {
-        page = 1
-    }
-    if pagesizestr := c.QueryParam("pagesize"); pagesizestr != "" {
-        if pagesize, err = strconv.ParseUint(pagesizestr, 10, 32); err != nil {
-            return
-        } 
-        if pagesize < 1 {
-            pagesize = 1
-        }
-        if pagesize > 100 {
-            pagesize = 100
-        }
-    } else {
-        pagesize = 100
-    }
+	var page, pagesize uint64
+	if pagestr := c.QueryParam("page"); pagestr != "" {
+		if page, err = strconv.ParseUint(pagestr, 10, 32); err != nil {
+			return
+		}
+		if page < 1 {
+			page = 1
+		}
+	} else {
+		page = 1
+	}
+	if pagesizestr := c.QueryParam("pagesize"); pagesizestr != "" {
+		if pagesize, err = strconv.ParseUint(pagesizestr, 10, 32); err != nil {
+			return
+		}
+		if pagesize < 1 {
+			pagesize = 1
+		}
+		if pagesize > 100 {
+			pagesize = 100
+		}
+	} else {
+		pagesize = 100
+	}
 
-    w := new(Where)
-    if err = c.Bind(w); err != nil {
-        return
-    }
-    if err = c.Validate(w); err != nil {
-        return
-    }
+	w := new(Where)
+	if err = c.Bind(w); err != nil {
+		return
+	}
+	if err = c.Validate(w); err != nil {
+		return
+	}
 
-    var l []User
-    if l, err = w.Pull(pagesize*(page-1), pagesize); err != nil {
-        return
-    }
-    if len(l) == 0 {
-        l = make([]User, 0)
-    }
+	var l []User
+	if l, err = w.Pull(pagesize*(page-1), pagesize); err != nil {
+		return
+	}
+	if len(l) == 0 {
+		l = make([]User, 0)
+	}
 
-    return c.JSON(http.StatusOK, l)
+	return c.JSON(http.StatusOK, l)
 }
 
+func count(c echo.Context) (err error) {
+    var count uint64
+    count, err = UserCount()
+    return c.JSON(http.StatusOK, Count{count})
+}
