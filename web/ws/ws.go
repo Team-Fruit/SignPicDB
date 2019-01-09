d175e9cad1da2a31f0297dd598b8417cac99ab0f
sjcl <jrhigasinihonn.0601@gmail.com>
2019-01-09 15:44:20 +0900

    websocket ping

--- web/ws/ws.go
+++ web/ws/ws.go
@@ -10,27 +10,13 @@ import (
 	"github.com/Team-Fruit/SignPicDB/web/models"
 )
 
-const (
-	// Time allowed to write a message to the peer.
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message from the peer.
-	pongWait = 60 * time.Second
-	
-	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10
-)
-
-var (
-	newline = []byte{'\n'}
-	space   = []byte{' '}
-)
-
 type Client struct {
 	hub  *Hub
 	conn *websocket.Conn
 	send chan []byte
 }
+
+const pingPeriod = 54 * time.Second
 
 var (
 	upgrader = websocket.Upgrader{
@@ -41,12 +27,7 @@ var (
 	AnalyticsChan = make(chan models.AnalyticsData)
 )
 
-// writePump pumps messages from the hub to the websocket connection.
-//
-// A goroutine running writePump is started for each connection. The
-// application ensures that there is at most one writer to a connection by
-// executing all writes from this goroutine.
-func (c *Client) writePump() {
+func (c *Client) pingTicker() {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
 		ticker.Stop()
@@ -54,32 +35,7 @@ func (c *Client) writePump() {
 	}()
 	for {
 		select {
-		case message, ok := <-c.send:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if !ok {
-				// The hub closed the channel.
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
-			w.Write(message)
-
-			// Add queued chat messages to the current websocket message.
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.send)
-			}
-
-			if err := w.Close(); err != nil {
-				return
-			}
 		case <-ticker.C:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
@@ -88,13 +44,13 @@ func (c *Client) writePump() {
 }
 
 func (c *Client) broadcast() {
-  for {
-    select {
-    case p := <-AnalyticsChan:
-      b, _ := json.Marshal(p)
-      c.hub.broadcast <- b
-    }
-  }
+	for {
+		select {
+		case p := <-AnalyticsChan:
+			b, _ := json.Marshal(p)
+			c.hub.broadcast <- b
+		}
+	}
 }
 
 func ServeWs(hub *Hub, c echo.Context) error {
@@ -105,7 +61,7 @@ func ServeWs(hub *Hub, c echo.Context) error {
 	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
 	client.hub.register <- client
 
-	go client.writePump()
+	go client.pingTicker()
 	go client.broadcast()
 
 	return nil
