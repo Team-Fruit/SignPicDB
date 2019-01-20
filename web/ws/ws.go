2d89be500b3d2b4950d1ff6f362701d3f5a0b867
sjcl <jrhigasinihonn.0601@gmail.com>
2019-01-20 22:01:09 +0900

    fix broken websocket

--- web/ws/ws.go
+++ web/ws/ws.go
@@ -16,9 +16,14 @@ type Client struct {
 	send chan []byte
 }
 
-const pingPeriod = 54 * time.Second
+const (
+	pingPeriod = 54 * time.Second
+	writeWait = 10 * time.Second
+)
 
 var (
+	newline = []byte{'\n'}
+
 	upgrader = websocket.Upgrader{
 		CheckOrigin: func(r *http.Request) bool {
 			return true
@@ -35,7 +40,32 @@ func (c *Client) pingTicker() {
 	}()
 	for {
 		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
 		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
